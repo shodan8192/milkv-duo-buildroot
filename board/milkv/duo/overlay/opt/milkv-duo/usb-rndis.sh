761f6846a98be224691dada8d03ef11f8a98866c
shodan8192 <23244044+shodan8192@users.noreply.github.com>
2023-11-20 15:23:14 +0100

    refactor: rename USB scripts, config file cleanup

--- board/milkv/duo/overlay/opt/milkv-duo/usb-rndis.sh
+++ board/milkv/duo/overlay/opt/milkv-duo/usb-rndis.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-/opt/milkv-duo/uhubon.sh device >> /tmp/rndis.log 2>&1
-/opt/milkv-duo/run_usb.sh probe rndis >> /tmp/rndis.log 2>&1
-/opt/milkv-duo/run_usb.sh start rndis >> /tmp/rndis.log 2>&1
+/opt/milkv-duo/usb-hub-ctrl.sh device >> /tmp/rndis.log 2>&1
+/opt/milkv-duo/usb-run.sh probe rndis >> /tmp/rndis.log 2>&1
+/opt/milkv-duo/usb-run.sh start rndis >> /tmp/rndis.log 2>&1
 
 sleep 0.5
 ifconfig usb0 192.168.42.1
